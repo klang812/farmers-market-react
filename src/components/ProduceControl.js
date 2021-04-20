511db537195633f5dcd81f1f971715e3a75987ff
Kerry Lang <klang812@gmail.com>
2021-04-20 10:39:11 -0700

    add local state produce
    Co-authored-by: Andy Lawson <alawson89@gmail.com>
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>

--- src/components/ProduceControl.js
+++ src/components/ProduceControl.js
@@ -10,60 +10,26 @@ constructor(props) {
   };
 }
 
-// monthSwitch = (params) => {
-//   switch (params) {
-//     case params == 'January':
-//       return this.state.month[0];
-//       break;
-//     case params == 'February':
-//       return this.state.month[1];
-//       break;
-//     case params == 'March':
-//       return this.state.month[2];
-//       break;
-//     case params == 'April':
-//       return this.state.month[3];
-//       break;
-//     case params == 'May':
-//       return this.state.month[4];
-//       break;
-//     case params == 'June':
-//       return this.state.month[5];
-//       break;
-//     case params == 'July':
-//       return this.state.month[6];
-//       break;
-//     case params == 'August':
-//       return this.state.month[7];
-//       break;
-//     case params == 'September':
-//       return this.state.month[8];
-//       break;
-//     case params == 'October':
-//       return this.state.month[9];
-//       break;
-//     case params == 'November':
-//       return this.state.month[10];
-//       break;
-//     case params == 'December':
-//       return this.state.month[11];
-//       break;
-//     default:
-//       return null;
-//   }
-// }
+handleClick = () => {
+  this.setState(prevState => ({
+    month: !prevState.month
+  }));
+}
 
 render() {
   let currentlyVisibleState = null;
+  let buttonText = null;
   if (this.state.month) {
     currentlyVisibleState = <ProduceList />
+    buttonText = "Back";
+  } else {
+    buttonText = "Show Produce List";
   }
-
 
   return(
     <>
     {currentlyVisibleState}
-    {/* {this.monthSwitch(params)} */}
+    <button onClick={this.handleClick}>{buttonText}</button>
     </>
   );
 }
